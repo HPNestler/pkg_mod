512d01a09d7204d4cfbc50ffd914c3097bdfcde9
HPNestler <peter.nestler@gmx.de>
2021-10-06 23:26:58 -0700

    Prop Annotation

--- chemistry_psql.py
+++ chemistry_psql.py
@@ -4,8 +4,8 @@ import psycopg2
 
 def SmilesInsert(smi_in):
     
-    db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
-    #db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
+    #db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
+    db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
     cur = db_chem.cursor()
     sql = 'INSERT INTO public.structures (smiles) VALUES(%s);'
     #print(sql)
@@ -37,3 +37,29 @@ def FPInsertMorgan(fpn, smi):
     cur = db_chem.cursor()
     sql = 'INSERT INTO public.structures (smiles) VALUES(%s);'
     #print(sql)
+    
+# expects a tuple or list of tuples
+
+def StructPropInsert(sp):
+    
+    #db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
+    db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
+    cur = db_chem.cursor()
+    sql = 'INSERT INTO public.structure_properties (property_type_id, structure_id, property_value) VALUES(%s, %s, %s);'
+    #print(sql)
+    #smi_in = smi_t
+    #print(smi_in)
+
+    print(sp)
+    try:
+        cur.execute(sql, (sp[0], sp[1], sp[2]))
+        db_chem.commit()
+        print('Inserted:', sp)
+    except Exception as error:
+        print("Property Exist: ", sp) #, error)
+        cur.close()
+        db_chem.close()
+        #db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
+        db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
+        cur = db_chem.cursor() 
+        sql = 'INSERT INTO public.structure_properties (property_type_id, structure_id, property_value) VALUES(%s, %s, %s);'
