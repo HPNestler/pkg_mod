9ce2c8ca5591ac491fd3686ce091ae2a7aac6f7a
HPNestler <31317039+HPNestler@users.noreply.github.com>
2022-03-11 10:54:40 -0800

    Chem_PSQL

--- chemistry_psql.py
+++ chemistry_psql.py
@@ -23,8 +23,8 @@ def SmilesInsert(smi_in):
             print("Structure Exist: ", i) #, error)
             cur.close()
             db_chem.close()
-            db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
-            #db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
+            #db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
+            db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
             cur = db_chem.cursor() 
             sql = 'INSERT INTO public.structures(smiles) VALUES(%s);'
             
@@ -32,8 +32,8 @@ def SmilesInsert(smi_in):
             
 def FPInsertMorgan(fpn, smi):
            
-    db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
-    #db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
+    #db_chem = psycopg2.connect(host = "localhost", dbname="Chemistry", user="postgres", password="postgres")
+    db_chem = psycopg2.connect(host = "192.168.86.31", dbname="Chemistry", user="postgres", password="postgres")
     cur = db_chem.cursor()
     sql = 'INSERT INTO public.structures (smiles) VALUES(%s);'
     #print(sql)
